9df207746151d4ec5c4ed02596299e92b2c80588
Imtious-Coder <iarafat386@gmail.com>
2023-03-30 06:13:28 +0600

    Transaction section updated

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -6,27 +6,27 @@ import "./Transaction.scss";
 const Transaction = () => {
   return (
     <>
-      <section className="transaction-wrapper py-10">
+      <section className="transaction-wrapper py-10 sm:py-28 px-3 sm:px-0">
         <div className="container mx-auto">
-          <div className="grid grid-cols-3 gap-10">
-            <h1 className="col-span-3 md:col-span-1 text-center sm:text-start">
+          <div className="grid grid-cols-3 gap-10 sm:gap-20">
+            <h1 className="Integral col-span-3 md:col-span-1 text-center sm:text-start">
               The amazing NFT art of the world here
             </h1>
-            <div className="col-span-3 flex md:col-span-1 gap-3 px-5 sm:px-0">
+            <div className="col-span-3 flex md:col-span-1 gap-3.5 px-5 sm:px-0">
               <img src={Image1} alt="" />
               <div>
-                <h6 className="mb-2">Fast Transaction</h6>
-                <p>
+                <h6 className="mb-3 DM">Fast Transaction</h6>
+                <p className="DM">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aliquam etiam viverra tellus imperdiet.
                 </p>
               </div>
             </div>
-            <div className="col-span-3 flex md:col-span-1 gap-3 px-5 sm:px-0">
+            <div className="col-span-3 flex md:col-span-1 gap-3.5 px-5 sm:px-0">
               <img src={Image2} alt="" />
               <div>
-                <h6 className="mb-2">Growth Transaction</h6>
-                <p>
+                <h6 className="mb-3 DM">Growth Transaction</h6>
+                <p className="DM">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aliquam etiam viverra tellus
                 </p>
