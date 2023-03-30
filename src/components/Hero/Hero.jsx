a49d1e134d4e140b804e39212cd9d810ec180ca9
Imtious-Coder <iarafat386@gmail.com>
2023-03-30 06:00:13 +0600

    Hero section updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,39 +9,43 @@ import "./Hero.scss";
 const Hero = () => {
   return (
     <>
-      <section className="hero-wrapper py-14 px-5">
+      <section className="hero-wrapper py-5 sm:pt-14 sm:pb-28 px-5">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 xl:gap-24">
             <div className="col-span-1">
-              <h1 className="mb-10 text-center sm:text-start">
+              <h1 className="Integral mb-6 text-center sm:text-start">
                 Discover, and collect Digital Art NFTs
               </h1>
-              <h6 className="mb-10 sm:pr-20 text-center sm:text-start">
+              <h6 className="DM mb-10 sm:pr-20 text-center sm:text-start">
                 Digital marketplace for crypto collectibles and non-fungible
                 tokens (NFTs). Buy, Sell, and discover exclusive digital assets.
               </h6>
-              <button className="button1 hero-cta-btn mb-10 mx-auto sm:mx-0">
+              <button className="Averta button1 hero-cta-btn mb-8 mx-auto sm:mx-0">
                 Explore Now
               </button>
-              <div className="count-up flex flex-wrap justify-center sm:justify-start gap-5">
+              <div className="count-up flex flex-wrap justify-center sm:justify-start gap-6 pt-0.5">
                 <div>
-                  <h2>98k+</h2>
-                  <p>Artwork</p>
+                  <h2 className="Integral">98k+</h2>
+                  <p className="DM">Artwork</p>
                 </div>
                 <div>
-                  <h2>12k+</h2>
-                  <p>Ausction</p>
+                  <h2 className="Integral">12k+</h2>
+                  <p className="DM">Auction</p>
                 </div>
                 <div>
-                  <h2>15k+</h2>
-                  <p>Artist</p>
+                  <h2 className="Integral">15k+</h2>
+                  <p className="DM">Artist</p>
                 </div>
               </div>
             </div>
             <div className="col-span-1 images relative">
               <div className="px-5 relative">
-                {/* stamp */}
-                <img src={Stamp} className="card-stamp hidden" alt="stamp" />
+                {/* stamp image */}
+                <img
+                  src={Stamp}
+                  className="hidden lg:block card-stamp"
+                  alt="stamp_image"
+                />
                 <div className="relative">
                   {data?.map((card, index) => {
                     return (
@@ -56,6 +60,7 @@ const Hero = () => {
                         } lg:absolute`}
                       >
                         <div className="relative card overflow-hidden bg-white">
+                          {/* card image */}
                           <img
                             src={card.image}
                             alt="card"
@@ -64,12 +69,17 @@ const Hero = () => {
                             } card-main-image h1`}
                           />
                           {/* card details */}
-                          <div className="absolute top-0 card-details p-3 sm:p-10">
+                          <div
+                            className={`${
+                              index === 0 ? "block" : "block lg:hidden"
+                            } absolute top-0 card-details p-3 sm:p-7`}
+                          >
                             <div className="relative">
-                              <h4 className="card-title mb-2 text-center sm:text-start">
+                              {/* card header */}
+                              <h4 className="DM card-title mb-2 text-center sm:text-start">
                                 Abstr Gradient NFT
                               </h4>
-                              <div className="flex justify-center sm:justify-start gap-4 items-center">
+                              <div className="flex justify-center sm:justify-start gap-3.5 items-center">
                                 <img
                                   src={Profile}
                                   className="card-profile-image"
@@ -78,19 +88,19 @@ const Hero = () => {
                                 <p className="card-profile-name">Arkhan17</p>
                               </div>
                               {/* card footer */}
-                              <div className="flex flex-col sm:flex-row justify-center gap-3 sm:gap-0 sm:justify-between items-center card-footer px-5 py-2 sm:py-0 mt-20 sm:mt-0">
-                                <div className="">
+                              <div className="flex flex-col sm:flex-row justify-center gap-3 sm:gap-0 sm:justify-between items-center card-footer px-5 py-3.5 sm:py-3.5 mt-20 sm:mt-0">
+                                <div className="DM">
                                   <p className="mb-2 footer-title">
                                     Current Bid
                                   </p>
-                                  <div className="flex items-center gap-2">
+                                  <div className="flex items-center gap-2.5">
                                     <p className="icon"> {card.nftLogo}</p>
-                                    <p className="footer-info">{card.nft}</p>
+                                    <p className="footer-nft">{card.nft}</p>
                                   </div>
                                 </div>
-                                <div>
+                                <div className="DM">
                                   <p className="mb-2 footer-title">Ends in</p>
-                                  <p className="footer-info">
+                                  <p className="footer-time">
                                     {card.endingTime}
                                   </p>
                                 </div>
