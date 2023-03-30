2701da83c12d8af4b136eb4a3e29538865ccbdf1
Imtious-Coder <iarafat386@gmail.com>
2023-03-30 07:18:59 +0600

    Top collection section updated

--- src/components/TopCollections/TopCollections.jsx
+++ src/components/TopCollections/TopCollections.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BsPatchCheckFill } from "react-icons/bs";
 import { SiEthereum } from "react-icons/si";
-import Image1 from "../../assets/images/top-collections/image1.png";
+import Image from "../../assets/images/top-collections/image.png";
 import Profile from "../../assets/images/top-collections/profile.png";
 import { middleCards, topCards } from "../../utils/TopCollectionsData";
 import "./TopCollections.scss";
@@ -9,39 +9,42 @@ import "./TopCollections.scss";
 const TopCollections = () => {
   return (
     <>
-      <section className="top-collection-wrapper py-10 sm:py-20 px-5 md:px-0">
+      <section className="top-collection-wrapper py-10 sm:py-32 px-5 md:px-0">
         <div className="container mx-auto">
-          <div className="grid grid-cols-3 gap-5">
+          <div className="grid grid-cols-3 gap-5 xl:gap-12">
+            {/* left card */}
             <div className="col-span-3 flex flex-col sm:flex-row lg:flex-col lg:col-span-1 left-card">
-              <img src={Image1} alt="main_image" className="mx-auto mb-7" />
+              <img src={Image} alt="main_image" className="mx-auto mb-8" />
+              {/* left card details */}
               <div className="flex sm:pl-10 lg:pl-0 flex-col lg:flex-row justify-center xl:justify-between gap-5 lg:gap-10 xl:gap-0 card-details">
-                <div className="flex flex-col sm:flex-row items-center gap-2">
+                <div className="flex flex-col sm:flex-row items-center gap-2.5">
                   <img
                     src={Profile}
                     alt="profile_image"
                     className="profile-image"
                   />
                   <div>
-                    <p className="name mb-1 text-center sm:text-start">
+                    <p className="DM name mb-1 text-center sm:text-start">
                       The Futr Abstr
                     </p>
-                    <p className="stock text-center sm:text-start">
+                    <p className="DM stock text-center sm:text-start">
                       10 in the stock
                     </p>
                   </div>
                 </div>
                 <div>
-                  <p className="bid mb-1 text-center sm:text-start">
+                  <p className="DM bid mb-2 text-center sm:text-start">
                     Highest Bid
                   </p>
                   <div className="flex items-center justify-center sm:justify-start gap-2">
                     <SiEthereum className="icon" />
-                    <p className="amount">0.25 ETH</p>
+                    <p className="DM nft">0.25 ETH</p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-span-3 lg:col-span-1 flex flex-col sm:flex-row flex-wrap lg:flex-col gap-10 lg:gap-5 middle-cards lg:border-r-2 mt-5 lg:mt-0">
+            {/* middle cards */}
+            <div className="col-span-3 lg:col-span-1 flex flex-wrap justify-center sm:justify-start lg:flex-col gap-10 lg:gap-5 middle-cards border-right1 mt-5 lg:mt-0">
               {middleCards?.map(
                 ({ image, name, nft, profileImage, count, button }, index) => {
                   return (
@@ -54,27 +57,27 @@ const TopCollections = () => {
                         alt="card_image"
                         className="card-image"
                       />
-                      <div>
-                        <h6 className="name mb-2 text-center sm:text-start">
+                      <div className="mt-1">
+                        <h6 className="DM name mb-2 text-center sm:text-start">
                           {name}
                         </h6>
-                        <div className="flex items-center gap-3">
+                        <div className="flex items-center gap-2">
                           <img
                             src={profileImage}
                             alt="profile"
                             className="profile-image"
                           />
-                          <button className="amount-btn">
+                          <button className="DM nft-btn">
                             <SiEthereum className="icon" /> {nft}
                           </button>
-                          <p className="count">{count}</p>
+                          <p className="DM count ml-0.5">{count}</p>
                         </div>
                         <button
                           className={`${
                             button === "button1"
                               ? "button1 cta-btn"
                               : "button2 cta-btn-2"
-                          } mt-3 mx-auto sm:mx-0`}
+                          } Averta mt-4 mx-auto sm:mx-0`}
                         >
                           Place a bid
                         </button>
@@ -84,9 +87,12 @@ const TopCollections = () => {
                 }
               )}
             </div>
-            <div className="col-span-3 flex flex-col items-center lg:col-span-1 top-cards">
-              <h2 className="mb-2">Top Collections over</h2>
-              <h3 className="mb-10">Last 7 days</h3>
+            {/* top collections */}
+            <div className="col-span-3 flex flex-col items-center md:items-start lg:col-span-1 top-cards">
+              <h2 className="Integral mb-2.5 text-center sm:text-start">
+                Top Collections over
+              </h2>
+              <h3 className="DM mb-8">Last 7 days</h3>
               {topCards?.map(
                 (
                   { id, image, name, rankColor, rank, nft, blueTick, border },
@@ -97,7 +103,7 @@ const TopCollections = () => {
                       key={index}
                       className={`${
                         border === true ? "card-border" : ""
-                      } grid grid-cols-1 sm:grid-cols-5 w-full gap-5 sm:gap-8 items-center mb-10 sm:mb-5 pb-5`}
+                      } flex flex-wrap justify-end sm:justify-center lg:grid lg:grid-cols-4 gap-5 items-center mb-10 sm:mb-5 pb-5`}
                     >
                       <p className="number">{id}</p>
                       <div className="block mx-auto">
@@ -113,18 +119,18 @@ const TopCollections = () => {
                         </div>
                       </div>
                       <div>
-                        <h6 className="name mb-2 text-center sm:text-start">
+                        <h6 className="DM name mb-2.5 text-center sm:text-start">
                           {name}
                         </h6>
-                        <div className="flex nft items-center justify-center sm:justify-start gap-1">
+                        <div className="flex flex-row lg:flex-col xl:flex-row nft items-center justify-center sm:justify-start gap-0.5">
                           <SiEthereum className="icon" />
-                          <p>{nft}</p>
+                          <p className="Poppins nft">{nft}</p>
                         </div>
                       </div>
                       <p
                         className={`${
                           rankColor === "green" ? "rank-green" : "rank-red"
-                        } text-center sm:text-end `}
+                        } Poppins text-center sm:text-end xl:ml-10`}
                       >
                         {rank}
                       </p>
